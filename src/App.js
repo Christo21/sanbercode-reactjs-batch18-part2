358dde8bc00976fb18c5ed5de88d1430cdec0fbf
dr4co <christoper21jo@gmail.com>
2020-10-09 00:31:12 +0700

    Tugas-9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form Pembelian Buah</h1>
+      <form>
+        <div className="form-group">
+          <div className="label"><b>Nama Pelanggan</b></div>
+          <input className="field" type="text" name="name" />
+        </div>
+        <div className="form-group">
+          <div className="label"><b>Daftar Item</b></div>
+          <div className="field">
+            <input type="checkbox" className="checkbox" name="semangka" value="Semangka" />
+            <label for="semangka"> Semangka</label><br />
+            <input type="checkbox" className="checkbox" name="jeruk" value="Jeruk" />
+            <label for="jeruk"> Jeruk</label><br />
+            <input type="checkbox" className="checkbox" name="nanas" value="Nanas" />
+            <label for="nanas"> Nanas</label><br />
+            <input type="checkbox" className="checkbox" name="salak" value="Salak" />
+            <label for="salak"> Salak</label><br />
+            <input type="checkbox" className="checkbox" name="anggur" value="Anggur" />
+            <label for="anggur"> Anggur</label>
+          </div>
+        </div>
+
+        <input className="form-button" type="submit" value="Kirim" />
+      </form>
     </div>
   );
 }
